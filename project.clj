9612124a623c00c20ed88b32bb44481f5f0da744
Lucas Severo <lvspais2@gmail.com>
2019-05-27 19:00:25 -0300

    changing version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject liberagent/lein-template "0.1.8-SNAPSHOT"
+(defproject liberagent/lein-template "0.1.8"
   :description "An updated version of the liberagent template"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
